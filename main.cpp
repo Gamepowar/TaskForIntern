57c4aca30501cce218ec638d6e04a788e207e6a3
Sergey <gamepowar@mail.ru>
2020-08-16 07:03:29 +0300

    parabola ok

--- main.cpp
+++ main.cpp
@@ -8,24 +8,24 @@ int main(int argc, char** argv) {/*
 	return 0;*/
 	
 	
-/*	HWND hwnd = GetConsoleWindow(); //Берём ориентир на консольное окно (В нём будем рисовать)
+	HWND hwnd = GetConsoleWindow(); //Берём ориентир на консольное окно (В нём будем рисовать)
 	HDC dc = GetDC(hwnd); //Цепляемся к консольному окну
 	RECT window = {}; //Переменная window будет использована для получения ширины и высоты окна
 	HBRUSH brush; //Переменная brush - это кисть, она будет использоваться для закрашивания
- 	*/
- 	Point p1(250, 250); 
-	Point p2(300, 250); 
-	Point p3(270, 300);
+ 	
+ 	Point p1(300, 250); 
+	Point p2(300, 300); 
+	Point p3(600, 400);
  //	Circle circle(p1,p2,p3);
- 	Parabola parabola(p1,p2,p3);
+ 	Parabola parabola(p1,p2,p3, false);
  	std::cout << "x0 = " << parabola.getX0() << ", y0 = " << parabola.getY0() << ", p = " << parabola.getP() << std::endl;
        /*В зависимости от значений у dx,dy эллипс будет менять форму, поэксперементируйте*/
     int R = 1; //Радиус определяет размер
     int dx1=0, dy1=0, dx2=4, dy2=4;  //Если dx2 == dy2, то круг, иначе эллипс
     int X = parabola.getX0(), Y = parabola.getY0(); //Начальные координаты
-  /*  Ellipse(dc, X+dx1, Y+dy1, X+dx2+R, Y+dy2+R);
+    Ellipse(dc, X+dx1, Y+dy1, X+dx2+R, Y+dy2+R);
     brush = CreateSolidBrush(RGB(255,0,0));
-    for(double i = 0; i < 100; i+=0.1){
+    for(double i = -500; i < 500; i+=3){
         X = (int)parabola.getX(i);
         Y = (int)parabola.getY(i);
         SelectObject(dc,brush);
@@ -43,12 +43,17 @@ int main(int argc, char** argv) {/*
     X = p3.x;
     Y = p3.y;
     Ellipse(dc, X+dx1, Y+dy1, X+dx2+R, Y+dy2+R);
+    X = parabola.getX0();
+	Y = parabola.getY0();
+	brush = CreateSolidBrush(RGB(255,255,255));
+    SelectObject(dc,brush);
+	Ellipse(dc, X+dx1, Y+dy1, X+dx2+R, Y+dy2+R);
 		 
     DeleteObject(brush);
 	ReleaseDC(hwnd, dc); //Освобождаем общий или оконный (не влияющий на класс или локальность) контекст устpойства, делая его доступным для дpугих пpикладных задач.
  
  
 	std::cin.get();
-	std::cin.get();*/
+	
 }
 
--- parabola.hpp
+++ parabola.hpp
@@ -2,7 +2,7 @@
 #include "soleq.h"
 #include <vector>
 class Parabola : public Curve{
-	private:
+/*	private:
 	 	
 		double koefX0(Point p1, Point p2){
 			return p2.y + 2 * p2.x * p1.y - p1.y;
@@ -19,7 +19,7 @@ class Parabola : public Curve{
 		double koefD(Point p1, Point p2){
 			return p1.x * p1.x * p2.y - p2.x * p2.x * p1.y;
 		}
-		
+		*/
 	protected:
 		
 		bool vertical;
@@ -40,10 +40,10 @@ class Parabola : public Curve{
 			p.push_back(p1);
 			p.push_back(p2);
 			p.push_back(p3);
-			for(int i = 0, j = 1; i < p.size(); i++, j++){
+		/*	for(int i = 0, j = 1; i < p.size(); i++, j++){
 				if(j == p.size()) j = 0;
 				if(vertical){
-					temp.push_back(this->koefX0(p[i], p[j]));
+					temp.push_back(this->koefX0(p[i].x, p[j]));
 					temp.push_back(this->koefY0(p[i], p[j]));
 				}
 				else{
@@ -52,6 +52,22 @@ class Parabola : public Curve{
 				}
 				temp.push_back(this->koefT(p[i], p[j]));
 				temp.push_back(this->koefD(p[i], p[j]));
+				equation.push_back(temp);
+				temp.clear();
+			}*/
+			for(int i = 0, j = 1; i < p.size(); i++, j++){
+				if(vertical){
+					temp.push_back(p[i].x * p[i].x);
+					temp.push_back(p[i].x);
+					temp.push_back(1);
+					temp.push_back(p[i].y);
+				}
+				else{
+					temp.push_back(p[i].y * p[i].y);
+					temp.push_back(p[i].y);
+					temp.push_back(1);
+					temp.push_back(p[i].x);
+				}
 				equation.push_back(temp);
 				temp.clear();
 			}
@@ -65,20 +81,25 @@ class Parabola : public Curve{
 					if(a[i][j] != 0 && j != a[i].size() - 1) isOneEq = false;
 				}
 			}
-			
+			double a1,b1,c1;
+			a1 = a[0][a[0].size()-1];
+			b1 = a[1][a[0].size() - 1];
+			c1 = a[2][a[0].size()-1];
 			std::cout <<"test\n"; 
 			if(!isOneEq){
 				throw std::invalid_argument("\nReshenie ne odno\n");
 			}
 			if(vertical){
-				x0 = a[0][a[0].size() - 1];
-				y0 = a[1][a[1].size() -1];
+			/*	x0 = -a[1][a[0].size() - 1] /(2 * a[0][a[0].size()-1]);
+				y0 = (a[1][a[0].size() - 1] * a[1][a[0].size() - 1] - 4 * a[0][a[0].size()-1]) - a[2][a[0].size()-1];*/
+				x0 = -b1 / (2 * a1);
+				y0 = -(b1*b1 - 4 * a1 * c1) / (4 * a1); 
 			}
 			else{
-				x0 = a[1][a[1].size() - 1];
-				y0 = a[0][a[0].size() - 1];
+				y0 = -b1 / (2 * a1);
+				x0 = -(b1*b1 - 4 * a1 * c1) / (4 * a1); ;
 			}
-			this->p = pow(p1.x - x0, 2) / (2 * (p1.y - y0));
+			this->p = 1/ (2 * a1);
 			
 			
 		}
@@ -88,13 +109,13 @@ class Parabola : public Curve{
 		}
 		
 		double getX(double t){
-			if (vertical)	return t;
-			else	return t * t / (2 * p);
+			if (vertical)	return t + x0;
+			else	return (t * t )/ (2 * p) + x0;
 		}
 		
 		double getY(double t){
-			if(vertical)	return t * t / (2 * p);
-			else 	return t;
+			if(vertical)	return t * t / (2 * p) + y0;
+			else 	return t + y0;
 		}
 		
 		virtual bool isClosed(){
